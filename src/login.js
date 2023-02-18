358e9df9fbf7b160c666ebf45c0e02312e3ea59a
Evar-SF27 <evarfomuso29@gmail.com>
2023-02-18 20:44:11 +0100

    added authstate provider

--- src/login.js
+++ src/login.js
@@ -1,6 +1,11 @@
-import { useRef, useEffect, useState } from 'react'
+import { useRef, useEffect, useState, useContext } from 'react'
+import AuthContext from './context/authProvider'
+
+const LOGIN_URL = '/auth'
 
 const Login = () => {
+    const { setAuth } = useContext(AuthContext)
+
     const userRef = useRef()
     const errRef = useRef()
 
@@ -19,48 +24,65 @@ const Login = () => {
 
     const handleSubmit = async (e) => {
         e.preventDefault()
+        setSuccess(true)
+        setUsername('')
+        setPassword('')
     }
 
   return (
-    <section>
-        <h1 className="title">Login Form</h1>
-        <p ref={errRef} className={errMessage ? "errmsg" : "offscreen"} aria-live="assertive">
-            {errMessage}
-        </p>
-        <form onSubmit={handleSubmit}>
-            <label htmlFor="username">Username:</label>
-            <input
-                type="text"
-                id="username"
-                ref={userRef}
-                autoComplete="off"
-                onChange={(e) => setUsername(e.target.value)} 
-                value={username}
-                required
-            />
-            <label htmlFor="password">Password:</label>
-            <input
-                type="password"
-                id="password"
-                onChange={(e) => setPassword(e.target.value)} 
-                value={password}
-                required
-            />
-            {/* Submit button */}
-            <button className="submitBtn" disabled={!username || !password}>
-                Sign In
-            </button>
-            {/* Sign In Info section */}
-            <p className="info">
-                I do not have an account.&nbsp;
-                <span className="line">
-                {/* Put a router link */}
-                {/* eslint-disable-next-line jsx-a11y/anchor-is-valid */}
-                <a href="#">Sign Up</a>
-                </span>
-            </p>
-        </form>
-    </section>
+    <>
+        {success ? (
+            <section>
+                <h1>You are successfully logged in!</h1>
+                <br />
+                <p>
+                    {/* To be replaced with React Router Link */}
+                    {/* eslint-disable-next-line jsx-a11y/anchor-is-valid */}
+                    <a href="#">Go to Home</a>
+                </p>
+            </section>
+        ) : (
+            <section>
+                <h1 className="title">Login Form</h1>
+                <p ref={errRef} className={errMessage ? "errmsg" : "offscreen"} aria-live="assertive">
+                    {errMessage}
+                </p>
+                <form onSubmit={handleSubmit}>
+                    <label htmlFor="username">Username:</label>
+                    <input
+                        type="text"
+                        id="username"
+                        ref={userRef}
+                        autoComplete="off"
+                        onChange={(e) => setUsername(e.target.value)} 
+                        value={username}
+                        required
+                    />
+                    <label htmlFor="password">Password:</label>
+                    <input
+                        type="password"
+                        id="password"
+                        onChange={(e) => setPassword(e.target.value)} 
+                        value={password}
+                        required
+                    />
+                    {/* Submit button */}
+                    <button className="submitBtn" disabled={!username || !password}>
+                        Sign In
+                    </button>
+                    {/* Sign In Info section */}
+                    <p className="info">
+                        I do not have an account.&nbsp;
+                        <span className="line">
+                        {/* Put a router link */}
+                        {/* eslint-disable-next-line jsx-a11y/anchor-is-valid */}
+                        <a href="#">Sign Up</a>
+                        </span>
+                    </p>
+                </form>
+            </section>
+        )}
+    </>
   )
 }
 

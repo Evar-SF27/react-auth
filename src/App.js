11f2395592fe01ce409c0a5cb661ab394ce238c5
Evar-SF27 <evarfomuso29@gmail.com>
2023-02-21 20:26:19 +0100

    persistent login

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { Admin, Editor, Home, Layout, LinkPage, Login, Lounge, Missing, Register, RequireAuth, Unauthorised } from './components'
+import { Admin, Editor, Home, Layout, LinkPage, Login, Lounge, Missing, PersistLogin, Register, RequireAuth, Unauthorised } from './components'
 import { Routes, Route } from 'react-router-dom'
 
 const ROLES = {
@@ -17,17 +17,19 @@ function App() {
         <Route path="linkpage" element={<LinkPage />} />
         <Route path="unauthorised" element={<Unauthorised />} />
         {/* Protected Routes */}
-        <Route element={<RequireAuth allowedRoles={[ROLES.User]}/>}>
-          <Route path="/" element={<Home />} />
-        </Route>
-        <Route element={<RequireAuth allowedRoles={[ROLES.Editor]}/>}>
-          <Route path="editor" element={<Editor />} />
-        </Route>
-        <Route element={<RequireAuth allowedRoles={[ROLES.Admin]}/>}>
-          <Route path="admin" element={<Admin />} />
-        </Route>
-        <Route element={<RequireAuth allowedRoles={[ROLES.Editor, ROLES.Admin]}/>}>
-          <Route path="lounge" element={<Lounge />} />
+        <Route element={<PersistLogin />}>
+          <Route element={<RequireAuth allowedRoles={[ROLES.User]}/>}>
+            <Route path="/" element={<Home />} />
+          </Route>
+          <Route element={<RequireAuth allowedRoles={[ROLES.Editor]}/>}>
+            <Route path="editor" element={<Editor />} />
+          </Route>
+          <Route element={<RequireAuth allowedRoles={[ROLES.Admin]}/>}>
+            <Route path="admin" element={<Admin />} />
+          </Route>
+          <Route element={<RequireAuth allowedRoles={[ROLES.Editor, ROLES.Admin]}/>}>
+            <Route path="lounge" element={<Lounge />} />
+          </Route>
         </Route>
         {/* Catch all */}
         <Route path="missing" element={<Missing />} />
